0e47d6da94a06675c2424d7414fe6f3fec63d180
ankitghoshthecreator <baruipara01@gmail.com>
2025-02-16 00:14:42 +0530

    hand tracking with fps showing

--- handTrackingMin.py
+++ handTrackingMin.py
@@ -6,8 +6,40 @@ print("hand tracking")
 
 cap= cv2.VideoCapture(0)
 
+
+mpHands=mp.solutions.hands
+hands=mpHands.Hands()
+mpDraw=mp.solutions.drawing_utils
+
+
+pTime=0
+cTime=0
+
 while True:
     success, img= cap.read()  #this will produce the frames
+
+    imgRGB =cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result= hands.process(imgRGB)
+
+    #print(result.multi_hand_landmarks)
+
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+
+    cv2.putText(img, str(int(fps)), (80, 100), cv2.FONT_HERSHEY_DUPLEX, 4, (255, 255,255))
+
+
+
+
+
+
+
+
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
